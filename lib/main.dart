a4a460bdfa8a1c4dab3e49e076c54f665d9a347a
leo-mack <leo.m4ck@gmail.com>
2025-11-07 00:52:07 +0000

    Styled Buttons and Maximum/Minimum amount disabled buttons

--- lib/main.dart
+++ lib/main.dart
@@ -70,13 +70,21 @@ Widget build(BuildContext context) {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: _increaseQuantity,
+                onPressed: _quantity < widget.maxQuantity ? _increaseQuantity : null,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white
+                ),
                 child: const Text('Add'),
                 ),
                 ElevatedButton(
-                  onPressed: _decreaseQuantity,
+                  onPressed: _quantity > 0 ? _decreaseQuantity : null,
+                  style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white
                   child: const Text('Remove'),
-                  ),
+                  )
+                ),
             ],
           ),
         ],
